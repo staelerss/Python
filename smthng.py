10b53a71f955712bdfdc379e0f12f2c384a00d58
Anatolii Kirichenko <anatoliykirichenko01@gmail.com>
2018-09-27 13:11:56 +0000

    Made some changes in smthng.py

--- smthng.py
+++ smthng.py
@@ -1,3 +1,16 @@
 from math import sqrt
 a = []
-n 
+cur = int(input())
+while cur != 0:
+    a.append(cur)
+    cur = int(input())
+print(a)
+a.append(0)
+n = len(a)
+S = float (sum(a) / n)
+summator = 0
+for i in range(n):
+    delta = a[i] - S
+    summator += delta
+sumator = float(summator / (n - 1))
+print (sqrt(summator))
