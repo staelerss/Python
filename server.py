4d1dcc647fc3eabfbcbf3ec349393ead1c93eb93
Anatolii Kirichenko <anatoliykirichenko01@gmail.com>
2018-10-19 12:38:36 +0000

    Added link to a webpage

--- server.py
+++ server.py
@@ -5,18 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    with app.test_request_context():
+        link = url_for('hello_user', username='Anatolii Host')
+        return render_template('index.html', link=link)
 
 @app.route('/user/')
 @app.route('/user/<path:username>')
 def hello_user(username=None):
     numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-    return render_template('user.html', username=username, numbers=numbers)
+    return render_template(
+            'user.html',
+            username=username,
+            numbers=numbers
+    )
 
-
-with app.test_request_context():
-    print("http://localhost" + url_for('hello_user', username='Anatolii Host'))
-
-if __name__ == '__main__':
-    app.run('0.0.0.0', debug=True)
+    if __name__ == '__main__':
+        app.run('0.0.0.0', debug=True)
