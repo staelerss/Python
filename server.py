cced3b7a80eb12a92d3c4bb26c94664b4b0cd5ec
Anatolii Kirichenko <anatoliykirichenko01@gmail.com>
2018-10-19 12:29:46 +0000

    Added index.html

--- server.py
+++ server.py
@@ -1,21 +1,22 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "Country road, take me home!" \
-           "To the place I belong"
+    return render_template('index.html')
 
 
+@app.route('/user/')
 @app.route('/user/<path:username>')
-def hello_user(username):
-    return "Hello %s" % username
+def hello_user(username=None):
+    numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    return render_template('user.html', username=username, numbers=numbers)
+
 
 with app.test_request_context():
     print("http://localhost" + url_for('hello_user', username='Anatolii Host'))
-
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
