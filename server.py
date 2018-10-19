3f79304eb27e47054e50ff1345bd4ccc0042db32
Anatolii Kirichenko <anatoliykirichenko01@gmail.com>
2018-10-19 13:15:28 +0000

    0.5v of smthng

--- server.py
+++ server.py
@@ -3,21 +3,35 @@ from flask import Flask, url_for, render_template
 app = Flask(__name__)
 
 
+def generate_links():
+    with app.test_request_context():
+        link_Anatolii = url_for('hello_user', username='Anatolii Host')
+        link_index = url_for('index')
+        link_Gregg = url_for('hello_user', username="Gregg")
+        links = (
+            ("Anatolii's", link_Anatolii),
+            ("Index's", link_index),
+            ("Gregg's", link_Gregg)
+        )
+    return links
+
+
 @app.route('/')
 def index():
-    with app.test_request_context():
-        link = url_for('hello_user', username='Anatolii Host')
-        return render_template('index.html', link=link)
+    links = generate_links()
+    return render_template('index.html', links=links)
+
 
 @app.route('/user/')
 @app.route('/user/<path:username>')
 def hello_user(username=None):
-    numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    links = generate_links()
     return render_template(
-            'user.html',
-            username=username,
-            numbers=numbers
+        'user.html',
+        username=username,
+        link=links
     )
 
-    if __name__ == '__main__':
-        app.run('0.0.0.0', debug=True)
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', debug=True)
