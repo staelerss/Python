01be231f441cc8810a32527be81a860c72c376e0
Anatolii Kirichenko <anatoliykirichenko01@gmail.com>
2018-12-20 13:04:06 +0000

    Added seperate py file for some functions

---
+++ repository/shop.py
@@ -0,0 +1,61 @@
+import pymysql.cursors
+
+connection = pymysql.connect(
+    host='localhost',
+    user='root',
+    password='',
+    db='shop',
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+
+def find_category(category_id):
+    with connection.cursor() as cursor:
+        cursor.execute('''
+        SELECT 
+            id,
+            title,
+            Description,
+            Image_URL
+        FROM category
+        WHERE id = %s
+        ''', (category_id,))
+        category = cursor.fetchone()
+
+    return category
+
+
+def find_all_categories():
+    with connection.cursor() as cursor:
+        cursor.execute("""
+        SELECT 
+            id,
+            title,
+            Description,
+            Image_URL
+        FROM category
+        """)
+        categories = cursor.fetchall()
+
+    return categories
+
+
+def find_products_by_category(category_id):
+    with connection.cursor() as cursor:
+        cursor.execute('''
+        SELECT
+            p.id,
+            p.title,
+            description,
+            price,
+            cat_id,
+            i.id AS image_id,
+            i.title AS image_title
+            FROM products p
+            LEFT JOIN image i ON p.id = i.product_id
+            WHERE cat_id = %s
+        ''', (category_id,))
+        products = cursor.fetchall()
+
+        return products
