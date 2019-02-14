32908f482587ab4c3cfd6d785a2be980af153fb4
Anatoliy Kirichenko <anatoliykirichenko01@gmail.com>
2019-02-14 12:10:25 +0000

    Main functions are working

--- static/cart.js
+++ static/cart.js
@@ -1,34 +1,31 @@
-const quantity = document.getElementById('amount');
-const quantity2 = document.getElementById('amount2');
-buttons = document.getElementsByClassName('pl');
-buttons2 = document.getElementsByClassName('second');
-increment = function () {
-    quantity.value = parseInt(quantity.value) + 1;
-}
-reduction = function () {
-    const number = parseInt(quantity.value);
-    quantity.value = number != 0 ? number - 1 : 0;
-}
-increment2 = function () {
-    quantity2.value = parseInt(quantity2.value) + 1;
-}
-reduction2 = function () {
-    const number = parseInt(quantity2.value);
-    quantity2.value = number != 0 ? number - 1 : 0;
-}
-Array.from(buttons).forEach((button) => {
-    if (button.getElementsByClassName('fa-plus').length) {
-        button.addEventListener('click', increment);
-    }
-    if (button.getElementsByClassName('fa-minus').length) {
-        button.addEventListener('click', reduction);
-    }
+const decrement = a => parseInt(a) ? a - 1: 0;
+const increment = a => parseInt(a) + 1;
+
+const cartPositions = Array.from(document.querySelectorAll('.cart-position'));
+console.log(cartPositions);
+cartPositions.forEach((cartPosition) => {
+    const price = parseInt(cartPosition.querySelector('.price').innerHTML);
+    const amount = cartPosition.querySelector('.amount');
+
+    const plusButton = cartPosition.querySelector('.plus-button');
+    const minusButton = cartPosition.querySelector('.minus-button');
+
+    const sum = cartPosition.querySelector('.sum');
+    sum.innerHTML = parseInt(amount.value) * price;
+
+    const total = cartPosition.querySelector('.total');
+    /*total.innerHTML = parseInt(sum.value) +*/
+
+    plusButton.addEventListener('click', () => {
+        const amountValue = increment(amount.value);
+        sum.innerHTML = amountValue * price;
+        amount.value = amountValue;
+    })
+
+    minusButton.addEventListener('click', () => {
+        const amountValue = decrement(amount.value);
+        sum.innerHTML = amountValue * price;
+        amount.value = amountValue;
+    })
 })
-Array.from(buttons2).forEach((button) => {
-    if (button.getElementsByClassName('pls').length) {
-        button.addEventListener('click', increment2);
-    }
-    if (button.getElementsByClassName('mns').length) {
-        button.addEventListener('click', reduction2);
-    }
-})
+
