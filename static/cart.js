84275749fdcf546877447e77c88ddd8ed699b22f
Anatoliy Kirichenko <anatoliykirichenko01@gmail.com>
2019-04-18 12:43:56 +0000

    Localstorage is finally

--- static/cart.js
+++ static/cart.js
@@ -1,84 +1,100 @@
-const decrement = a => parseInt(a) ? a - 1 : 0;
-const increment = a => parseInt(a) + 1;
-
-const cartPositions = Array.from(document.querySelectorAll('.cart-position'));
-console.log(cartPositions);
-cartPositions.forEach((cartPosition) => {
-    const price = parseInt(cartPosition.querySelector('.price').innerHTML);
-    const amount = cartPosition.querySelector('.amount');
-
-    const plusButton = cartPosition.querySelector('.plus-button');
-    const minusButton = cartPosition.querySelector('.minus-button');
-
-    const sum = cartPosition.querySelector('.sum');
-    sum.innerHTML = parseInt(amount.value) * price;
-
-    const total = cartPosition.querySelector('.total');
-    let a = 0;
-    console.log('table'.length);
-    for (let i = 0; i < 'table'.length; i++) {
-        a += parseInt(cartPosition.querySelector('.sum'));
-    }
-    // total.innerHTML = parseInt(cartPosition.querySelector('.sum');
-    plusButton.addEventListener('click', () => {
-        const amountValue = increment(amount.value);
-        sum.innerHTML = amountValue * price;
-        amount.value = amountValue;
-    })
-
-    minusButton.addEventListener('click', () => {
-        const amountValue = decrement(amount.value);
-        sum.innerHTML = amountValue * price;
-        amount.value = amountValue;
-    })
-})
-
-cart = localStorage.getItem('cart');
-const buyButtons = document.querySelectorAll('.buyButton');
-Array.from(buyButtons).forEach(buyButton => buyButton.addEventListener('click', (e) => {
-    e.preventDefault();
-
-
-}))
-
-const cart = {
-    deliveryPrice: 120,
-    deliveryMethod: 'post',
-    positions: [
-        {id: 1, title: "Phone 2", price: 120, count: 2, sum: 0},
-        {id: 3, title: "Phone 3", price: 120, count: 3, sum: 0},
-        {id: 4, title: "Phone 4", price: 120, count: 1, sum: 0},
-    ]
+const increment = (cart, idPosition) => {
+    cart.positions[idPosition].amount += 1
+    localStorage.setItem('cart', JSON.stringify(cart))
+    render(cart)
+}
+const decrement = (cart, idPosition) => {
+    cart.positions[idPosition].amount && (cart.positions[idPosition].amount -= 1)
+    localStorage.setItem('cart', JSON.stringify(cart))
+    render(cart)
 }
 
-const renderCart = () => {
-    const priceTd = document.createElement('td')
-    priceTd.innerHTML = '100'
+const cart = JSON.parse(localStorage.getItem('cart'))
+console.log(cart)
 
-    const trs = cart.positions.map(position => {
+const arrayFromObject = (obj) => {
+    return Object.keys(obj).map(key => obj[key])
+}
+
+
+function addTobasket(productId) {
+    let cart = JSON.parse(localStorage.getItem('cart'))
+    if (cart === null) {
+        cart = {
+            positions: {},
+        }
+    }
+
+    cart.positions = {
+        ...cart.positions,
+        [productId]:
+            getProductFromServer(productId),
+    }
+    console.log(cart)
+    localStorage.setItem('cart',
+        JSON.stringify(cart))
+}
+
+const getProductFromServer = (productId) => {
+    return products[productId]
+}
+
+const buttons = document.getElementsByClassName('add-button')
+
+Array.from(buttons).forEach(button => {
+    button.addEventListener('click', (e) => {
+        addTobasket(e.target.dataset.id);
+    })
+})
+const generateCartTable = (cart) => {
+    const trs = arrayFromObject(cart.positions).map(position => {
         position.sum = position.price * position.amount
 
-        return `<tr class="cart-position">
-            <td>Name</td>
+        return `
+        <tr class="cart-position" id="${position.id}">
+            <td>${position.title}</td>
             <td class="price">${position.price}</td>
             <td>
                 <button class="minus-button">-</button>
-                <input type="text" value="${position.amount}" class="amount"
+                <input type="text" value="${position.amount}" class="amount">
                 <button class="plus-button">+</button>
             </td>
             <td class="sum">${position.sum}</td>
-        </tr>`
+        </tr>
+        `
     })
 
-    const totalSum = cart.positions.redue((sum, position) => sum + position.sum, 0)
 
-    const totalTr = `<tr>
-                        <td colspan="3"></td>
-                        <td class="total">0</td>
-                    </tr>`
+    const totalSum = arrayFromObject(cart.positions).reduce((sum, position) => sum + position.sum, 0)
 
-    return `<table>${trs}${totalTr}</table>`
+    const totalTr = `
+        <tr>
+            <td colspan="3"></td>
+            <td class="total">${totalSum}</td>
+        </tr>`
+
+    const table = document.createElement('table')
+    table.innerHTML = trs + totalTr
+
+    Array.from(table.querySelectorAll('button')).forEach(button => {
+        const id = button.closest('.cart-position').id
+        button.addEventListener('click', () => button.className === 'plus-button'
+            ? increment(cart, id)
+            : decrement(cart, id)
+        )
+    })
+
+    return table;
 }
+
+const render = (cart) => {
+    const cartDiv = document.getElementById('cart')
+    const table = cartDiv.querySelector('table')
+    table && cartDiv.removeChild(table)
+    cartDiv.appendChild(generateCartTable(cart))
+}
+
+render(cart)
 
 
 
